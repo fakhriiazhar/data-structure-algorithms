40f5aab3867a3f5042f6c8a9925d9391c7e10bac
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-26 12:30:40 +0800

    Create list3.py

---
+++ Built in data input & output/list/list3.py
@@ -0,0 +1,16 @@
+print("berikut adalah hasil contoh Fragment dari(54)._add__(21 :)\n", (54).__add__(21))
+
+print(range(10))
+print(list(range(10)))
+print(list(range(5, 10)))
+print(list(range(5,15,3)))
+print(list(range(10,1,-1)))
+
+myName = "Fakhrii Azhar"
+print(len(myName))
+print(list(myName))
+print(myName.upper)
+print(myName.center(10))
+print(myName.find('a'))
+print(myName.split('a'))
+print(myName.count("Fakhrii Azhar"))
