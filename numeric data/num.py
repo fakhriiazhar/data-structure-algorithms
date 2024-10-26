af8a796c6b1323610e7d879d7e112a57e01f33fb
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-26 11:03:44 +0800

    Create num.py

---
+++ numeric data/num.py
@@ -0,0 +1,12 @@
+x = 11
+y = 69
+
+Ha = x + y; print(Ha)
+Hb = Ha = 10.5;print("HB =", Hb)
+Hc = int(Hb);print(Hc)
+Hd = Ha / Hb;print(Hd)
+isinstance(Hd, int)
+isinstance(Hd, float)
+if isinstance(Hd, int):
+    print("Hd adalah bilangan integer!")
+else: print("Hd adalah bilangan float!")
