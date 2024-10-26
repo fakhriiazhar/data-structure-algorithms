8b07a1bb6fce94f25384e7843c33723a842d6d04
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-26 12:27:36 +0800

    Create var.py

---
+++ Built in data input & output/var.py
@@ -0,0 +1,18 @@
+#variable
+Mynim = str(2311102441169)
+
+Mynim_10th = Mynim[9]
+Mynim_11th = Mynim[-3]
+i = 11
+Mynim_12th = Mynim[i]
+Mynim_13th = Mynim[i+1]
+a = int(Mynim_10th + Mynim_11th)
+b = float(Mynim_12th + Mynim_13th)
+
+print(Mynim)
+print(Mynim_10th)
+print(Mynim_11th)
+print(Mynim_12th)
+print(Mynim_13th)
+print(a)
+print("nilai float b = ", b)
